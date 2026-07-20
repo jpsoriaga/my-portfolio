25db5e2a2191a2986557decd275dc256f7a94ded
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-07-21 00:00:27 +0800

    update

--- client/src/components/RecentProject/RecentProject.tsx
+++ client/src/components/RecentProject/RecentProject.tsx
@@ -3,7 +3,7 @@
 export default function RecentProject() {
     return(
         <section className="relative isolate w-full overflow-hidden pt-10">
-            <div className="relative z-10 mx-auto w-full max-w-[1120px] px-6 sm:px-8 md:px-10 lg:px-14 xl:px-16 2xl:px-10">
+            <div className="relative z-10 mx-auto w-full max-w-[1120px] bg-red-50 px-6 sm:px-8 md:px-10 lg:px-14 xl:px-16 2xl:px-10">
                 <div>
                     test
                 </div>
---
+++ client/src/components/RecentProject/RecentProjectCard.tsx
@@ -0,0 +1,11 @@
+
+
+export default function RecentProjectCard() {
+    return(
+        <>
+            <div>
+                
+            </div>
+        </>
+    );
+}
