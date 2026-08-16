de9bc0923b1809c90ce901ce29e096f48b9ed446
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-08-16 15:28:08 +0800

    update

--- client/src/components/Personal/MusicTaste.tsx
+++ client/src/components/Personal/MusicTaste.tsx
@@ -9,6 +9,7 @@ export default function MusicTaste() {
                     <div className="pointer-events-none absolute inset-0 z-10 rounded-xl ring-1 ring-white/10 transition duration-300 group-hover:ring-[#1db954]/45" />
                     <iframe
                         src="https://open.spotify.com/embed/track/2vWBUC9djv6BtiGlmKiQaH?utm_source=oembed"
+                        loading="lazy"
                         className="absolute left-0 top-0 h-full w-full border-0"
                         allow="clipboard-write *; encrypted-media *; fullscreen *; picture-in-picture *;"
                         title="Spotify track player"
